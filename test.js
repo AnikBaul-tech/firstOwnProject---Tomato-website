c45b9f144ada8a2f12b4538a716759f6f2878be5
Anik <anikbaul2@gmail.com>
2025-08-17 11:45:43 +0530

    foodItems array added

--- test.js
+++ test.js
@@ -63,14 +63,90 @@ const toggleBtn = document.getElementById('theme-toggle');
     localStorage.setItem('theme', theme);
   });
 
-  gl.viewport(0, 0, canvas.width, canvas.height);
-function resizeCanvas() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  gl.viewport(0, 0, canvas.width, canvas.height);
-}
-window.addEventListener('resize', resizeCanvas);
-resizeCanvas();
+document.addEventListener("DOMContentLoaded", () => {
+const foodItems = [
+  {
+    name: "Butter Chicken",
+    description: "Soft tender Chicken with the authentic spices of India",
+    image: "./images/food card images/butter_chicken.jpg",
+    type: "non-veg"
+  },
+  {
+    name: "Chicken Tikka",
+    description: "Spicy grilled Chicken cheese with spices",
+    image: "./images/food card images/chicken_tikka.jpg",
+    type: "non-veg"
+  },
+  {
+    name: "Paneer Tikka",
+    description: "Spicy grilled cottage cheese with veggies",
+    image: "./images/food card images/paneer_tikka.jpeg",
+    type: "veg"
+  },
+  {
+    name: "Choco Lava Cake",
+    description: "Puffy outer layer with the burst of choco fountain inside",
+    image: "./images/food card images/choco_lava.jpg",
+    type: "veg"
+  },
+  {
+    name: "Mango Lassi",
+    description: "Smooth mango pulps with the soothing sweet texture",
+    image: "./images/food card images/mango_lassi.jpg",
+    type: "all"
+  },
+  {
+    name: "Butter Naan",
+    description: "Soft, fluffy Indian flatbread brushed with rich melted butter",
+    image: "./images/food card images/butter_naan.jpg",
+    type: "veg"
+  },
+  {
+    name: "Virgin Mojito",
+    description: "Gulp of the glacier, refreshing as the night shower",
+    image: "./images/food card images/virgin_mojito.jpg",
+    type: "all"
+  },
+  {
+    name: "Schezwan Momo",
+    description: "Spice with the flavours of hills, top notch choice for a quickee",
+    image: "./images/food card images/schezwan_momo.jpg",
+    type: "veg"
+  },
+  {
+    name: "Chicken Noodles",
+    description: "Chicken mashup with the Noodles",
+    image: "./images/food card images/chicken-noodles.avif",
+    type: "non-veg"
+  },
+  {
+    name: "Fish Fry",
+    description: "Touch of some sea with decent spices",
+    image: "./images/food card images/fish-cutlet.jpg",
+    type: "non-veg"
+  }
+];
+console.log(foodItems);
+const container = document.querySelector(".food-card-container"); // Make sure your HTML has this container
+
+foodItems.forEach(item => {
+  const card = document.createElement("div");
+  card.className = "food-card";
+  card.setAttribute("data-type", item.type);
+
+  card.innerHTML = `
+    <div class="food-info">
+      <div class="food-name national-park-bold">${item.name}</div>
+      <div class="food-description national-park-thin">${item.description}</div>
+    </div>
+    <div class="food-image">
+      <img src="${item.image}" alt="${item.name}">
+    </div>
+  `;
+
+  container.appendChild(card);
+});
+});
 
 function setFilter(type) {
   const cards = document.querySelectorAll('.food-card');
