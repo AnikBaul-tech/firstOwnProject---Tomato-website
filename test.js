d3be33db4bc4ddc322efa5b155affe68d440ff25
Anik <anikbaul2@gmail.com>
2025-08-18 20:42:57 +0530

    Search section activated

--- test.js
+++ test.js
@@ -66,65 +66,118 @@ const toggleBtn = document.getElementById('theme-toggle');
 document.addEventListener("DOMContentLoaded", () => {
 const foodItems = [
   {
+    name: "Mutton Handi",
+    description: "Slow cooked Mutton with Indian Spices",
+    image: "./images/food card images/mutton_handi.avif",
+    type: "non-veg",
+    price: 350,
+    qty: "3 Pcs."
+  },
+  {
     name: "Butter Chicken",
     description: "Soft tender Chicken with the authentic spices of India",
     image: "./images/food card images/butter_chicken.jpg",
-    type: "non-veg"
+    type: "non-veg",
+    price: 250,
+    qty: "2 Pcs."
   },
   {
     name: "Chicken Tikka",
     description: "Spicy grilled Chicken cheese with spices",
     image: "./images/food card images/chicken_tikka.jpg",
-    type: "non-veg"
+    type: "non-veg",
+    price: 280,
+    qty: "1 Stick."
   },
   {
     name: "Paneer Tikka",
     description: "Spicy grilled cottage cheese with veggies",
     image: "./images/food card images/paneer_tikka.jpeg",
-    type: "veg"
+    type: "veg",
+    price: 230,
+    qty: "1 Stick."
   },
   {
     name: "Choco Lava Cake",
     description: "Puffy outer layer with the burst of choco fountain inside",
     image: "./images/food card images/choco_lava.jpg",
-    type: "veg"
+    type: "veg",
+    price: 150,
+    qty: "1 Pc."
   },
   {
     name: "Mango Lassi",
     description: "Smooth mango pulps with the soothing sweet texture",
     image: "./images/food card images/mango_lassi.jpg",
-    type: "all"
+    type: "all",
+    price: 100,
+    qty: "1 Glass"
   },
   {
     name: "Butter Naan",
     description: "Soft, fluffy Indian flatbread brushed with rich melted butter",
     image: "./images/food card images/butter_naan.jpg",
-    type: "veg"
+    type: "veg",
+    price: 150,
+    qty: "2 Pcs."
   },
   {
     name: "Virgin Mojito",
     description: "Gulp of the glacier, refreshing as the night shower",
     image: "./images/food card images/virgin_mojito.jpg",
-    type: "all"
+    type: "all",
+    price: 120,
+    qty: "1 Glass"
   },
   {
     name: "Schezwan Momo",
     description: "Spice with the flavours of hills, top notch choice for a quickee",
     image: "./images/food card images/schezwan_momo.jpg",
-    type: "veg"
+    type: "veg",
+    price: 180,
+    qty: "5 Pcs."
   },
   {
     name: "Chicken Noodles",
     description: "Chicken mashup with the Noodles",
     image: "./images/food card images/chicken-noodles.avif",
-    type: "non-veg"
+    type: "non-veg",
+    price: 230,
+    qty: "1 Full Plate"
   },
   {
     name: "Fish Fry",
     description: "Touch of some sea with decent spices",
     image: "./images/food card images/fish-cutlet.jpg",
-    type: "non-veg"
+    type: "non-veg",
+    price: 180,
+    qty: "1 Pc"
+  },
+  {
+    name: "Chicken Biriyani",
+    description: "fragrant, spiced rice dish layered with tender chicken caramelized onions and aromatic herbs",
+    image: "./images/food card images/chicken_biriyani.avif",
+    type: "non-veg",
+    price: 250,
+    qty: "1 Full Plate"
+  },
+  {
+    name: "Capuccino",
+    description: "Creamy coffee drink made with equal parts espresso, steamed milk, and milk foam.",
+    image: "./images/food card images/capuccino.jpg",
+    type: "all",
+    price: 180,
+    qty: "1 Cup"
+  },
+  {
+    name: "Fried Rice and Chili Chicken",
+    description: "Rice with the sauted veggies and tossed chicken with sauces",
+    image: "./images/food card images/fried_rice_chicken.jpg",
+    type: "non-veg",
+    price: 200,
+    qty: "1 Plate with 2Pcs. of Chicken"
   }
+  
 ];
 console.log(foodItems);
 const container = document.querySelector(".food-card-container"); // Make sure your HTML has this container
@@ -137,6 +190,7 @@ foodItems.forEach(item => {
   card.innerHTML = `
     <div class="food-info">
       <div class="food-name national-park-bold">${item.name}</div>
+      <div class="food-price-description national-park-thin">${item.price}₹ || Qty: ${item.qty}</div>
       <div class="food-description national-park-thin">${item.description}</div>
     </div>
     <div class="food-image">
@@ -155,3 +209,25 @@ function setFilter(type) {
     card.style.display = (type === 'all' || type === foodType) ? 'flex' : 'none';
   });
 }
+
+const searchInput = document.querySelector('.search_input');
+const notKeyWord =['||','₹','Qty',':','Pcs','.'];
+
+searchInput.addEventListener('input', () => {
+  const keyword = searchInput.value.toLowerCase().trim();
+  const cards = document.querySelectorAll('.food-card');
+
+  cards.forEach(card => {
+    const name = card.querySelector('.food-name').textContent.toLowerCase();
+    const desc = card.querySelector('.food-description').textContent.toLowerCase();
+    const price = card.querySelector('.food-price-description').textContent.toLowerCase();
+
+    const combinedText = `${name} ${desc} ${price}`;
+    
+      if (combinedText.includes(keyword)) {
+        card.style.display = 'flex';
+      } else {
+        card.style.display = 'none';
+      }
+  });
+});
